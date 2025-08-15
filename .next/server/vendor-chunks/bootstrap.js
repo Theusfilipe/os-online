a21b138cc847690fb376e7723297edb0bacab43e
RCSSUBAQUATICA <ti@rcssubaquatica.com.br>
2025-08-15 16:18:43 -0300

    feat: Implement Criar Ordem de Serviço modal with form handling
    - Added CriarOrdemServicoModal component for creating service orders.
    - Integrated form fields for various service order attributes.
    - Updated layout to include the new modal trigger and removed old modal code.
    - Enhanced state management for form data submission.
    - Included Bootstrap styling and functionality for modal display.

--- .next/server/vendor-chunks/bootstrap.js
+++ .next/server/vendor-chunks/bootstrap.js
@@ -21,6 +21,17 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 
 /***/ }),
 
+/***/ "(ssr)/./node_modules/bootstrap/dist/css/bootstrap.min.css":
+/*!***********************************************************!*\
+  !*** ./node_modules/bootstrap/dist/css/bootstrap.min.css ***!
+  \***********************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (\"a65e4fd8db9b\");\nif (false) {}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKHNzcikvLi9ub2RlX21vZHVsZXMvYm9vdHN0cmFwL2Rpc3QvY3NzL2Jvb3RzdHJhcC5taW4uY3NzIiwibWFwcGluZ3MiOiI7Ozs7QUFBQSxpRUFBZSxjQUFjO0FBQzdCLElBQUksS0FBVSxFQUFFLEVBQXVCIiwic291cmNlcyI6WyJDOlxcVXNlcnNcXHJjc3N1XFxEb2N1bWVudHNcXFdlYlNpdGVSQ1NcXG9yZGVuc09ubGluZVxcb3Mtb25saW5lXFxub2RlX21vZHVsZXNcXGJvb3RzdHJhcFxcZGlzdFxcY3NzXFxib290c3RyYXAubWluLmNzcyJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCBcImE2NWU0ZmQ4ZGI5YlwiXG5pZiAobW9kdWxlLmhvdCkgeyBtb2R1bGUuaG90LmFjY2VwdCgpIH1cbiJdLCJuYW1lcyI6W10sImlnbm9yZUxpc3QiOlswXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(ssr)/./node_modules/bootstrap/dist/css/bootstrap.min.css\n");
+
+/***/ }),
+
 /***/ "(ssr)/./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js":
 /*!****************************************************************!*\
   !*** ./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js ***!
--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,4 +1,5 @@
 'use client';
+import CriarOrdemServicoModal from '@/components/criarItemModal';
 
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { useState } from "react";
@@ -19,29 +20,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             <input className="form-control me-2" type="search" placeholder="" aria-label="Search"/>
             <button className="btn btn-outline-success" type="submit">Procurar</button>
           </form>
-          <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+          <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#criarOrdemModal">
             Criar
           </button>
         </div>
       </nav>
 
-          <div className="modal fade" id="exampleModal" tabIndex={-1} aria-hidden="true">
-            <div className="modal-dialog">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title">Modal title</h5>
-                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div className="modal-body">
-                        <p>Modal body text goes here.</p>
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" >Close</button>
-                        <button type="button" className="btn btn-primary" >Save changes</button>
-                    </div>
-                </div>
-            </div>
-          </div>
+      <CriarOrdemServicoModal />    
 
       <main>{children}</main>
     </div>
